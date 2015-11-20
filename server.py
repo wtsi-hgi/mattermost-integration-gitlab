1f542c6c4d5d11ed7e9eea5453e8bdc8326144be
NotSqrt <notsqrt@gmail.com>
2015-11-20 17:30:29 +0100

    Lint

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python Future imports
+from __future__ import unicode_literals, absolute_import, print_function
+
+# Python System imports
 import os
 import sys
 import requests
 import json
 import re
+
+
+# Third-party imports
 from flask import Flask
 from flask import request
 
@@ -10,8 +20,8 @@ app = Flask(__name__)
 
 USERNAME = 'gitlab'
 ICON_URL = 'https://gitlab.com/uploads/project/avatar/13083/gitlab-logo-square.png'
-MATTERMOST_WEBHOOK_URL = '' # Paste the Mattermost webhook URL you created here
-CHANNEL = '' # Leave this blank to post to the default channel of your webhook
+MATTERMOST_WEBHOOK_URL = ''  # Paste the Mattermost webhook URL you created here
+CHANNEL = ''  # Leave this blank to post to the default channel of your webhook
 
 PUSH_EVENT = 'push'
 ISSUE_EVENT = 'issue'
@@ -20,12 +30,13 @@ COMMENT_EVENT = 'note'
 MERGE_EVENT = 'merge_request'
 
 REPORT_EVENTS = {
-    PUSH_EVENT: False, # On pushes to the repository excluding tags
-    ISSUE_EVENT: True, # On creation of a new issue
-    TAG_EVENT: False, # On creation of tags
-    COMMENT_EVENT: True, # When a new comment is made on commits, merge requests, issues, and code snippets
-    MERGE_EVENT: True, # When a merge request is created
+    PUSH_EVENT: False,  # On pushes to the repository excluding tags
+    ISSUE_EVENT: True,  # On creation of a new issue
+    TAG_EVENT: False,  # On creation of tags
+    COMMENT_EVENT: True,  # When a new comment is made on commits, merge requests, issues, and code snippets
+    MERGE_EVENT: True,  # When a merge request is created
 }
+
 
 @app.route('/')
 def root():
@@ -35,6 +46,7 @@ def root():
 
     return "OK"
 
+
 @app.route('/new_event', methods=['POST'])
 def new_event():
     """
@@ -42,7 +54,7 @@ def new_event():
     """
 
     if request.json is None:
-        print 'Invalid Content-Type'
+        print('Invalid Content-Type')
         return 'Content-Type must be application/json and the request body must contain valid JSON', 400
 
     data = request.json
@@ -51,7 +63,7 @@ def new_event():
     text = ''
     base_url = ''
 
-    if REPORT_EVENTS[PUSH_EVENT] and  object_kind == PUSH_EVENT:
+    if REPORT_EVENTS[PUSH_EVENT] and object_kind == PUSH_EVENT:
         text = '%s pushed %d commit(s) into the `%s` branch for project [%s](%s).' % (
             data['user_name'],
             data['total_commits_count'],
@@ -168,7 +180,7 @@ def new_event():
         base_url = data['object_attributes']['target']['web_url']
 
     if len(text) == 0:
-        print 'Text was empty so nothing sent to Mattermost, object_kind=%s' % object_kind
+        print('Text was empty so nothing sent to Mattermost, object_kind=%s' % object_kind)
         return 'OK'
 
     if len(base_url) != 0:
@@ -177,6 +189,7 @@ def new_event():
     post_text(text)
 
     return 'OK'
+
 
 def post_text(text):
     """
@@ -193,23 +206,25 @@ def post_text(text):
         data['channel'] = CHANNEL
 
     headers = {'Content-Type': 'application/json'}
-    r = requests.post(MATTERMOST_WEBHOOK_URL, headers=headers, data=json.dumps(data))
+    resp = requests.post(MATTERMOST_WEBHOOK_URL, headers=headers, data=json.dumps(data))
 
-    if r.status_code is not requests.codes.ok:
-        print 'Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (MATTERMOST_WEBHOOK_URL, r.status_code, r.json())
+    if resp.status_code is not requests.codes.ok:
+        print('Encountered error posting to Mattermost URL %s, status=%d, response_body=%s' % (MATTERMOST_WEBHOOK_URL, resp.status_code, resp.json()))
+
 
 def fix_gitlab_links(base_url, text):
     """
     Fixes gitlab upload links that are relative and makes them absolute
     """
 
-    matches = re.findall('(\[[^]]*\]\s*\((/[^)]+)\))', text)
+    matches = re.findall(r'(\[[^]]*\]\s*\((/[^)]+)\))', text)
 
     for (replace_string, link) in matches:
         new_string = replace_string.replace(link, base_url + link)
         text = text.replace(replace_string, new_string)
 
     return text
+
 
 def add_markdown_quotes(text):
     """
@@ -226,6 +241,7 @@ def add_markdown_quotes(text):
 
     return '\n'.join(split_desc)
 
+
 if __name__ == "__main__":
     MATTERMOST_WEBHOOK_URL = os.environ.get('MATTERMOST_WEBHOOK_URL', '')
     CHANNEL = os.environ.get('CHANNEL', CHANNEL)
@@ -239,7 +255,7 @@ if __name__ == "__main__":
     REPORT_EVENTS[MERGE_EVENT] = os.environ.get('MERGE_TRIGGER', str(REPORT_EVENTS[MERGE_EVENT])) == 'True'
 
     if len(MATTERMOST_WEBHOOK_URL) == 0:
-        print 'MATTERMOST_WEBHOOK_URL must be configured. Please see instructions in README.md'
+        print('MATTERMOST_WEBHOOK_URL must be configured. Please see instructions in README.md')
         sys.exit()
 
     port = int(os.environ.get('PORT', 5000))
