dcc238d4415d110d971ed3a52303d0b493304a2d
NotSqrt <notsqrt@gmail.com>
2016-12-19 10:34:11 +0100

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="mattermost-integration-gitlab",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/NotSqrt/mattermost-integration-gitlab",
 
     author="NotSqrt",
